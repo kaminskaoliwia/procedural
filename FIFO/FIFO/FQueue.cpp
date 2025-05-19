4b611e1f57e5734d6777c3f4cfa65f6a84c06200
Oliwia <kaminskaoliwia32@gmail.com>
2025-05-19 19:59:44 +0200

    Poprawienie komunikatów

--- FIFO/FIFO/FQueue.cpp
+++ FIFO/FIFO/FQueue.cpp
@@ -3,41 +3,41 @@
 
 FQueue* FQCreate()                    // queue initiating  (NULL - fail)
 {
-  FQueue* pNew = ( FQueue*)malloc(sizeof(FQueue) ); // alokuje pamiêæ, pierwszy pusty wskaŸnik
-  if( !pNew ) // je¿eli nie ma miejsce
+  FQueue* pNew = ( FQueue*)malloc(sizeof(FQueue) ); // alokuje pamiÄ™Ä‡, pierwszy pusty wskaÅºnik
+  if( !pNew ) // jeÅ¼eli nie ma miejsce
   {
     return NULL;
   }
-  pNew->pHead = pNew->pTail = NULL; // wskaŸnik pNew wskazuj¹cy na pHead/pTail = NULL
+  pNew->pHead = pNew->pTail = NULL; // wskaÅºnik pNew wskazujÄ…cy na pHead/pTail = NULL
 
-  return pNew; // returnuje pNew typu wskaŸnik do FQueue
+  return pNew; // returnuje pNew typu wskaÅºnik do FQueue
 }
 
 int FQEmpty( FQueue* q ) // if FIFO empty returned 1 else 0
 {
-  return !q || !(q->pHead); /* FQueue* = wskaŸnik do struktury danych kolejki
-    !q, sprawdza czy wgl istnieje, a q->pHead sprawdza czy s¹ dane (kolejka mog³a zostaæ utworzona
-    ale niedodane zosta³y ¿adne dane)
+  return !q || !(q->pHead); /* FQueue* = wskaÅºnik do struktury danych kolejki
+    !q, sprawdza czy wgl istnieje, a q->pHead sprawdza czy sÄ… dane (kolejka mogÅ‚a zostaÄ‡ utworzona
+    ale niedodane zostaÅ‚y Å¼adne dane)
 
-    Strza³ka ma w celu dostanie siê do pHead przez wskaŸnik do struktury q, równowa¿ne jest:
+    StrzaÅ‚ka ma w celu dostanie siÄ™ do pHead przez wskaÅºnik do struktury q, rÃ³wnowaÅ¼ne jest:
     (*q).pHead
   */
 }
 
 int    FQEnqueue( FQueue* q, QINFO* p ) // insert new item at the end
 {
-  if( !q ) return 0; // je¿eli kolejka nie istnieje to nie
+  if( !q ) return 0; // jeÅ¼eli kolejka nie istnieje to nie
 
   //! memory allocation
   FQItem* pNew = (FQItem*)malloc(sizeof(FQItem) );
-  if( !pNew ) { // je¿eli nie ma miejsca
+  if( !pNew ) { // jeÅ¼eli nie ma miejsca
     return 0;
   }
 
   pNew-> pInfo = p; // nowy int k = x
-  pNew-> pNext = NULL; // jako, ¿e dodajemy na koniec, nastêpny wskaŸnik musi byæ pusty
+  pNew-> pNext = NULL; // jako, Å¼e dodajemy na koniec, nastÄ™pny wskaÅºnik musi byÄ‡ pusty
 
-  if( q->pHead == NULL) // je¿eli nie ma ¿adnego member 
+  if( q->pHead == NULL) // jeÅ¼eli nie ma Å¼adnego member 
   {
     q-> pHead = pNew; // to nasz nowy member jest wskazywany na przez pHead
   }
@@ -58,17 +58,17 @@ QINFO* FQDequeue( FQueue* q )        //! take out the first item
     return 0;
   }
 
-  FQItem* temp = q->pHead; // tworzymy nowy skaŸnik na pHead o nazwie temp
-  QINFO* value = temp->pInfo; // pobieramy wartoœæ key z tempa i nazywamy t¹ wartoœæ value
+  FQItem* temp = q->pHead; // tworzymy nowy skaÅºnik na pHead o nazwie temp
+  QINFO* value = temp->pInfo; // pobieramy wartoÅ›Ä‡ key z tempa i nazywamy tÄ… wartoÅ›Ä‡ value
   q->pHead = q->pHead->pNext; /* (*q).pHead = (*q).pHead.pNext, czyli naszym nowym pHead
-                                   nastêpna wartoœæ*/
+                                   nastÄ™pna wartoÅ›Ä‡*/
 
-  if ( !q->pHead ) {  // je¿eli kolejka stanie siê pusta
-    q->pTail = NULL; // (*q).pTail staje siê pusta
+  if ( !q->pHead ) {  // jeÅ¼eli kolejka stanie siÄ™ pusta
+    q->pTail = NULL; // (*q).pTail staje siÄ™ pusta
   }
-  free( temp ); // zwalniamy pamiêæ z tempa
+  free( temp ); // zwalniamy pamiÄ™Ä‡ z tempa
 
-  return value; // wartoœæ ju¿ usuniêtej wartoœci, która kiedyœ by³a pHead
+  return value; // wartoÅ›Ä‡ juÅ¼ usuniÄ™tej wartoÅ›ci, ktÃ³ra kiedyÅ› byÅ‚a pHead
 }
 
 void   FQClear( FQueue* q, void(__cdecl* freeMem)(const void*) )          // clears the queue
@@ -88,14 +88,14 @@ void   FQClear( FQueue* q, void(__cdecl* freeMem)(const void*) )          // cle
 void    FQRemove( FQueue** q, void (__cdecl* freeMem)(const void*) )         // clears the queue  (=QFClear()) and removes
 {
   FQClear( *q, freeMem );
-  free( *q ); // zwalnia wskaŸnik q       
+  free( *q ); // zwalnia wskaÅºnik q       
 }
 
 void    FQDel( FQueue* q )             //! removes only first item
 {
   if(FQEmpty( q ) )
   {
-    printf("\nERROR del: stack is empty!!!\n");
+    printf("\nFQDEL: Kolejka jest pusta.\n");
     return;
   }
 
@@ -108,7 +108,7 @@ void    FQDel( FQueue* q )             //! removes only first item
 void FQPrint(FQueue* q)
 {
   if (FQEmpty(q)) {
-    printf("Kolejka jest pusta.\n");
+    printf("\nFQPRINT: Kolejka jest pusta.\n");
     return;
   }
 
